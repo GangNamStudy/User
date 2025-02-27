8e888829c14c0e9591d488b2b97b4cce34120ed6
Jin-h0 <mir001125@naver.com>
2025-02-27 23:13:49 +0900

    feat: first commit

---
+++ src/test/java/com/Parking_management/Parking_management/ParkingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Parking_management.Parking_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
